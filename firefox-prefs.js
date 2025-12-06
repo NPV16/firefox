4fcfa1bec454d06a4bc3c3b1b6979ecfaef2f234
goyo123321a <goyo123321@mail.fresnostate.edu>
2025-12-06 21:47:24 +0800

    Update firefox-prefs.js

--- firefox-prefs.js
+++ firefox-prefs.js
@@ -1,12 +1,11 @@
+// 设置首页
 user_pref("browser.startup.homepage", "https://idx.google.com");
 user_pref("browser.startup.page", 1);
-
-user_pref("extensions.autoRefreshPage.enabled", true);
-user_pref("extensions.experiments.enabled", true);
-
+// 禁用首次运行向导
 user_pref("startup.homepage_welcome_url", "");
 user_pref("startup.homepage_welcome_url.additional", "");
-user_pref("browser.shell.checkDefaultBrowser", false);
-
+// 关键：允许从 addons.mozilla.org 安装扩展
+user_pref("xpinstall.signatures.required", false);
+user_pref("extensions.experiments.enabled", true);
+// 优化远程环境
 user_pref("gfx.webrender.all", false);
-user_pref("layers.acceleration.force-enabled", false);
